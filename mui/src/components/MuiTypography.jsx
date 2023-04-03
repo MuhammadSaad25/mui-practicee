d5e4b27d1c281180da7538a4ac5f3827b0e0d0a6
Shehzad <122433416+muhammadsaad402@users.noreply.github.com>
2023-04-04 03:38:47 +0500

    mui practice

---
+++ mui/src/components/MuiTypography.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+export const MuiTypography = () => {
+  return (
+    <div>
+      MuiTypography
+      <Typography></Typography>
+    </div>
+  );
+};
